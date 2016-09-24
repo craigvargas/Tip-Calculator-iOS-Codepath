c10f63a07b1844eec039666bf16824d9de7da37b
Craig Vargas <cvar@Craigs-MacBook-Pro.local>
2016-09-24 15:39:19 -0400

    ready for first submission

--- Tipster/SettingsViewController.swift
+++ Tipster/SettingsViewController.swift
@@ -8,10 +8,32 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController:
+    UIViewController,
+    UIPickerViewDataSource,
+    UIPickerViewDelegate{
+    
+    @IBOutlet weak var tipDefaultPicker: UIPickerView!
+    
+    @IBOutlet weak var defaultTipLabel: UILabel!
+    
+    var defaultTip: Int = 0
+    var defaultTipHundreds: Int = 0
+    var defaultTipTens: Int = 0
+    var defaultTipOnes: Int = 0
+    
+    var tipPickerArray = [
+        ["0","1"],
+        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        ]
+    //[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tipDefaultPicker.dataSource = self;
+        self.tipDefaultPicker.delegate = self;
 
         // Do any additional setup after loading the view.
     }
@@ -31,5 +53,105 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tipPickerArray[component].count;
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return tipPickerArray[component][row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        switch component {
+        case 0:
+            defaultTipHundreds = (Int(tipPickerArray[component][row])! ?? 0) * 100
+            print("switch hundreds: \(defaultTipHundreds)")
+            break
+        case 1:
+            defaultTipTens = (Int(tipPickerArray[component][row])! ?? 0) * 10
+            print("switch tens: \(defaultTipTens)")
+            break
+        case 2:
+            defaultTipOnes = (Int(tipPickerArray[component][row])! ?? 0) * 1
+            print("switch ones: \(defaultTipOnes)")
+            break
+        default:
+            break
+        }
+        printEachDigit();
+        defaultTip = defaultTipHundreds + defaultTipTens + defaultTipOnes
+        if defaultTip > 100 {
+            defaultTip = 100
+        }
+        defaultTipLabel.text = "\(defaultTip)%"
+        print("Inside user picked pickerview. DefaultTip = \(defaultTip)")
+    }
+    
+    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let titleData = tipPickerArray[component][row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont(name: "Georgia", size: 15.0)!,NSForegroundColorAttributeName:UIColor.whiteColor()])
+        return myTitle
+    }
+    
+    func printEachDigit(){
+        print("\(defaultTipHundreds), \(defaultTipTens), \(defaultTipOnes)")
+    }
+    
+    func saveDefault(){
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(defaultTip, forKey: "default_tip")
+        defaults.synchronize()
+    }
+    
+    //Initialize view
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        //Initialize default tip percentage to zero
+        defaultTip = 0
+        
+        //Get default tip from settings
+        defaultTip = defaults.integerForKey("default_tip")
+        
+        //Calculate the hunreths, tens, and ones digit values
+        let hundreds = Int(defaultTip/100)
+        defaultTipHundreds = hundreds * 100
+        let tens = Int(defaultTip/10)-hundreds*10
+        defaultTipTens = tens*10
+        let ones =
+            defaultTip - (hundreds*100) - (tens*10)
+        defaultTipOnes = ones
+        
+        //Set the picker values to teh users last default tip value
+        tipDefaultPicker
+            .selectRow(hundreds, inComponent: 0, animated: true)
+        tipDefaultPicker
+            .selectRow(tens, inComponent: 1, animated: true)
+        tipDefaultPicker
+            .selectRow(ones, inComponent: 2, animated: true)
+        
+        //Set up the Picker corners
+        tipDefaultPicker.layer.cornerRadius = 10;
+        tipDefaultPicker.layer.masksToBounds = true;
+        
+        //Set the default tip text
+        defaultTipLabel.text = "\(defaultTip)%"
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    //Save important items
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        saveDefault()
+    }
 }
--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var userControlsView: UIView!
+    
     @IBOutlet weak var tipSlider: UISlider!
     
     @IBOutlet weak var tipPctTopLabel: UILabel!
@@ -22,7 +24,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var customerBillField: UITextField!
     
-    var isBillFinalized: Bool = true
+    var billIsFinalized: Bool = true
+    var appIsAlreadyRunning: Bool = false
+    
+    var currencyFormatter = NSNumberFormatter()
+    var billFormatter = NSNumberFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,47 +45,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onTipPctChanged(sender: UISlider) {
-        let userTipPct = Int(sender.value)
-        tipPctTopLabel.text = "\(userTipPct)%"
-        tipPctBottomLabel.text = "\(userTipPct)%"
-        
-        let userBill = Double(customerBillField.text!) ?? 0
-        
-        calcTip(userBill, tipPct: userTipPct)
-        
-//        tipPctTopLabel.text = tipPctBottomLabel.text = String(format: "%d", userTipPct)
+        updateUi()
     }
     
     //User started typing in text field
     @IBAction func onCustomerBillBeginEditing(sender: UITextField) {
+        billIsFinalized=false
         sender.text = ""
-        calcTip(0, tipPct: 0)
+        updateUi()
     }
     
     //User is typing in Bill text field
     @IBAction func onCustomerBillChanged(sender: UITextField) {
-        let userTipPct = Int(tipSlider.value) ?? 0
-        let userBill = Double(customerBillField.text!) ?? 0
-        
-//        customerBillField.text = String(format: "$%.2f", userBill)
-        
-        calcTip(userBill, tipPct: userTipPct)
+        billIsFinalized=false
+        updateUi()
     }
     
     //User has finished typing in Bill text field
     @IBAction func onCustomerBillFinalized(sender: UITextField) {
-        isBillFinalized = true
-        
-        let userTipPct = Int(tipSlider.value) ?? 0
-        let userBill = Double(customerBillField.text!) ?? 0
-        
-        customerBillField.text = String(format: "$%.2f", userBill)
-        
-        calcTip(userBill, tipPct: userTipPct)
+        billIsFinalized=true
+        updateUi()
     }
     
     func calcTip(bill: Double, tipPct: Int){
-        
         let tipNotional = bill * Double(tipPct)/100
         let totalBill = bill + tipNotional
         
@@ -87,15 +75,69 @@ class ViewController: UIViewController {
         totalBillLabel.text = String(format: "$%.2f", totalBill)
     }
     
+    func updateUi(){
+        let userTipPct = Int(tipSlider.value)
+        let stringBill = customerBillField.text!
+        let userBill = formatBillToDouble(stringBill)
+        
+        let tipNotional = userBill * Double(userTipPct)/100
+        let totalBill = userBill + tipNotional
+        
+        tipNotionalLabel.text = currencyFormatter.stringFromNumber(tipNotional)
+        totalBillLabel.text = currencyFormatter.stringFromNumber(totalBill)
+        tipPctTopLabel.text = "\(userTipPct)%"
+        tipPctBottomLabel.text = "\(userTipPct)%"
+        if billIsFinalized{
+            var formattedBill = ""
+            billFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+            formattedBill = billFormatter.stringFromNumber(userBill)!
+            customerBillField.text = formattedBill
+        }
+    }
+    
+    func formatBillToDouble(bill: String) -> Double {
+        if bill == ""{return 0}
+        print("formatBillToDouble: \(billFormatter.numberFromString(bill))")
+
+        if bill[bill.startIndex] == "$"{
+            return Double(currencyFormatter.numberFromString(bill)!)
+        }else{
+            return Double(bill) ?? 0
+        }
+    }
+    
     //Initialize view
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        tipPctTopLabel.text = "\(tipSlider.value)%"
-        tipPctBottomLabel.text = "\(tipSlider.value)%"
-//        let defaults = NSUserDefaults.standardUserDefaults()
-//        let defaultTip = defaults.integerForKey("default_tip")
-//        tipSlider.setValue(Float(defaultTip), animated: true)
-        print("view will appear")
+        
+        userControlsView.layer.cornerRadius = 10
+        userControlsView.layer.masksToBounds = true
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let defaultTip = defaults.integerForKey("default_tip")
+        tipSlider.setValue(Float(defaultTip), animated: true)
+        billIsFinalized=false
+        updateUi()
+    }
+    
+    //Initialize view part 2
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        billFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        if appIsAlreadyRunning{
+            
+        }else{
+            //App has just started so make text field first responder
+            //Tell the program that the app is already running
+            customerBillField.delegate = self
+            customerBillField.becomeFirstResponder()
+            appIsAlreadyRunning = true
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
     }
     
 }
